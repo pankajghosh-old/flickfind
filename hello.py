fc520e61f381a0a0ea3b0bdab6222de31df3c566
Pankaj Ghosh <pankaj.ghosh@gmail.com>
2014-08-06 17:42:46 -0400

    added postgres

--- hello.py
+++ hello.py
@@ -1,11 +1,14 @@
 import os
 import json
+
 import requests
-
 import flask
-from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
+
 cache = {}
 imgcache = {}
 
