7f2579f3636e9575b915cb741f32615874320c87
Pankaj Ghosh <pankaj.ghosh@gmail.com>
2014-09-02 23:34:54 -0400

    added rotten tomatoes rating, not exposed in ui yet

--- external_api.py
+++ external_api.py
@@ -40,10 +40,21 @@ def do_search_from_rottentomatoes(search_term):
 
 		imdb_id = request_movie_dict.get('alternate_ids', {}).get('imdb', None)
 		poster_url = request_movie_dict.get('posters', {}).get('detailed', None)
-		if imdb_id:
+
+		critics_score = request_movie_dict['ratings'].get('critics_score', 0)
+		critics_rating = request_movie_dict['ratings'].get('critics_rating', None)
+		audience_score = request_movie_dict['ratings'].get('audience_score', 0)
+		audience_rating = request_movie_dict['ratings'].get('audience_rating', None)
+
+		if imdb_id and (critics_rating or audience_rating):
 			new_movie_dict['urlPoster'] = '/movie_poster?url='+poster_url
 			new_movie_dict['urlIMDB'] = get_imdb_url_from_id(imdb_id)
 			new_movie_dict['rating'] = get_imdb_rating(imdb_id)
-			new_movie_dict['title'] = request_movie_dict['title']		
+			new_movie_dict['title'] = request_movie_dict['title']
+			new_movie_dict['critics_score'] = critics_score
+			new_movie_dict['critics_rating'] = critics_rating
+			new_movie_dict['audience_score'] = audience_score
+			new_movie_dict['audience_rating'] = audience_rating
+
 			return_list.append(new_movie_dict)
 	return return_list
--- scrap.py
+++ scrap.py
@@ -13,6 +13,11 @@ def get_movie_list_from_rotten_tomatoes():
 	res = json.loads(requests.get(request_url).content)
 	print 'results count', res['total']
 	for movie_dict in res['movies']:
+		print 'ratings', movie_dict['ratings']
+		print 'critics score', movie_dict['ratings'].get('critics_score', 0)
+		print 'critics rating', movie_dict['ratings'].get('critics_rating', None)
+		print 'audience score', movie_dict['ratings'].get('audience_score', 0)
+		print 'audience rating', movie_dict['ratings'].get('audience_rating', None)
 		print 'title', movie_dict['title']
 		print 'imdb_id', movie_dict.get('alternate_ids', {}).get('imdb', None)
 		print 'detailed url poster', movie_dict.get('posters', {}).get('detailed', None)
