2884d44d7b28c3185ce2489d57284bdf13f0f0d8
Pankaj Ghosh <pankaj.ghosh@gmail.com>
2014-09-08 22:34:27 -0400

    add button working with reach;search still isnt

--- js/main_jxs.js
+++ js/main_jxs.js
@@ -2,11 +2,7 @@
 var SearchTerm = React.createClass({
   getInitialState:function(){
     return {isSelected:false};
-    // return {data:[{term:"termabc"}, {term:"termbcd"}]}
   },
-  // componentDidMount: function() {
-  //   this.handleClick();
-  // },
   render: function() {
     var classes = React.addons.classSet({
       'btn': true,
@@ -14,41 +10,19 @@ var SearchTerm = React.createClass({
       'btn-success': this.state.isSelected,
     });
     return (
-      <a className={classes} onChange={this.handleSelection}>
+      <a className={classes} parentClickHandle={this.handleSelection}>
       {this.props.term}
       </a>
       );
   },
-  handleSelection: function(term){
+  handleSelection: function(){
     console.log('child received event', term)
   }
-  // handleClick: function(event) {
-  //   console.log('child click event')
-  //   this.setState({isSelected:true})
-  //   getlinks(this.props.term);
-  //   console.log('child click event end')
-  // }
 });
 
 var SearchTermList = React.createClass({
-  getInitialState:function(){
-    return {data:[{term:"abc"},{term:"bcd"}]};
-    // return {data:[{term:"termabc"}, {term:"termbcd"}]}
-  },
-  componentDidMount: function() {
-    $.ajax({
-      url: this.props.url,
-      dataType: 'json',
-      success: function(data) {
-        this.setState({data: data});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
-  },
   render: function() {
-    var searchTermNodes = this.state.data.map(function (searchTerm) {
+    var searchTermNodes = this.props.data.map(function (searchTerm) {
       return (
         <SearchTerm term={searchTerm.term}>
         </SearchTerm>
@@ -61,33 +35,19 @@ var SearchTermList = React.createClass({
       );
   },
   handleClick: function(event) {
-    console.log('parent click event', event.target.text);
-    // console.log(this.props);
-    // if (this.props.onChange){
-    //   this.props.onChange(event.target.text);
-    // }
-    // console.log('called children events')
-    // console.log('parent click event', event.target.text);
-    // console.log(this.props.children);  // handleClick: function(event) {
-  //   console.log('child click event')
-  //   this.setState({isSelected:true})
-  //   getlinks(this.props.term);
-  //   console.log('child click event end')
-  // }
-
-  React.Children.forEach(this.props.children, function(child){
-    console.log(child.props.term, event.target.text);
-        // child.setState({ isSelected: child.props.term === event.target.text });
-        // child.parentClickHandler();
-      });
-}
+    React.Children.forEach(this.props.data, function(child){
+      // console.log(child.props, event.target.text);
+        // child.setState({ isSelected: child.props === event.target.text });
+        child.parentClickHandle();
+      });    
+  }
 });
 
-var SearchTermForm = React.createClass({
+var SearchTermBox = React.createClass({
   getInitialState: function() {
     return {data:[{term:"abc"},{term:"bcd"}]};
   },
-  componentDidMount: function() {
+  loadCommentsFromServer:function(){
     $.ajax({
       url: this.props.search_terms_url,
       dataType: 'json',
@@ -99,31 +59,38 @@ var SearchTermForm = React.createClass({
       }.bind(this)
     });
   },
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+  },
+  handleAddSearchTerm:function(){
+    var addsearchterm = this.refs.addsearchterm.getDOMNode().value.trim();
+    console.log('add search term clicked', addsearchterm)
+    $.ajax({
+      url: this.props.search_terms_url,
+      dataType: 'json',
+      type: 'POST',
+      data: {term:addsearchterm},
+      success: function(data) {
+        this.loadCommentsFromServer();
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
   render: function() {
     return (
       <div className="searchTermForm">
-        <h1>Search Terms</h1>
-        <SearchTermList data={this.state.data} />
-        <CommentForm />
+      <SearchTermList data={this.state.data} />
+      <input type="text" placeholder="Say something here..." ref="addsearchterm" />
+      <button id="addsearchtermbutton" onClick={this.handleAddSearchTerm}>Add Search Term</button>
       </div>
-    );
+      );
   }
 });
 
-// var CommentForm = React.createClass({
-//   render: function() {
-//     return (
-//       <form className="commentForm">
-//       <input type="text" placeholder="Your name" />
-//       <input type="text" placeholder="Say something..." />
-//       <input type="submit" value="Post" />
-//       </form>
-//       );
-//   }
-// });
-
 React.renderComponent(
-  <SearchTermForm search_terms_url="search_terms"/>,
+  <SearchTermBox search_terms_url="search_terms"/>,
   document.getElementById('selection_buttons_react')
   );
 
