5f4927b8f60f03671e359980baf1842358547489
Pankaj Ghosh <pankaj.ghosh@gmail.com>
2014-09-15 22:10:52 -0400

    got search terms working

--- js/search_jxs.js
+++ js/search_jxs.js
@@ -8,56 +8,26 @@ var SearchBox = React.createClass({
 	},
 	componentDidMount: function(){
 		var element = this.getDOMNode();
-		var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-					  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-					  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-					  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-					  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-					  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-					  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-					  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-					  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-					]; 
-		var substringMatcher = function(strs) {
-			return function findMatches(q, cb) {
-				var matches, substrRegex;
-
-		    // an array that will be populated with substring matches
-			    matches = [];
-
-			    // regex used to determine if a string contains the substring `q`
-			    substrRegex = new RegExp(q, 'i');
-
-			    // iterate through the pool of strings and for any string that
-			    // contains the substring `q`, add it to the `matches` array
-			    $.each(strs, function(i, str) {
-			    	if (substrRegex.test(str)) {
-			        // the typeahead jQuery plugin expects suggestions to a
-			        // JavaScript object, refer to typeahead docs for more info
-			        matches.push({ value: str });
-			    	}
-				});
-
-			cb(matches);
-			};
-		};
-		$(element).typeahead(
-		{
-			minLength: 1,
-			highlight: true,
-			hint:true
-		},
-		{
-			name: 'states',
-			displayKey: 'value',		  
-			source: substringMatcher(states)
+		var search_terms = new Bloodhound({
+		  datumTokenizer: function(d) { return Bloodhound.tokenizers.whitespace(d.term); },
+		  queryTokenizer: Bloodhound.tokenizers.whitespace,
+		  limit:10,
+		  prefetch:'./search_terms'
 		});
-console.log('componentDidMount called');
-},
-componentWillUnmount:function(){
-	console.log('componentWillUnmount called');
-}
-});
+		 
+		// kicks off the loading/processing of `local` and `prefetch`
+		search_terms.initialize();		
+		$(element).typeahead(null,
+		{
+			name: 'search_terms',
+			displayKey: 'term',		  
+			source: search_terms.ttAdapter()
+		});
+	},
+	componentWillUnmount:function(){
+		console.log('componentWillUnmount called');
+	}
+	});
 
 React.renderComponent(
 	<SearchBox/>,
